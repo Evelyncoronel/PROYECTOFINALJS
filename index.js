6d6e4a5cc5357cdc1101a2bb4ccf61aabc3f23ce
malbec <evelyn.ec.dc@gmail.com>
2022-04-13 20:25:21 -0300

    carrito en producto 1

--- index.js
+++ index.js
@@ -24,9 +24,9 @@ if (colores){
 let enStock = colores.find (function(color) {
     return color == "negro";
 });
-if (enStock) { 
+if(enStock) { 
     console.log ("enStock");
-} else {
+}else{
     console.log ("Sin stock");
 };
 
@@ -48,11 +48,11 @@ while (producto) {
     }
     if(producto == 'aruba'+ talleproducto == 'small' + colorProducto == 'negro'){ 
         alert('disponible');
-    }else(producto == 'aruba' + talleproducto == 'medium' + colorProducto == 'negro' ){
+    }else(producto == 'aruba' + talleproducto == 'medium' + colorProducto == 'negro'){
         alert('sin stock');
     }
 }
-console.log("producto");
+console.log("producto"); 
 
 //*Carrito con DOM*/
 let btn1 = document.getElementById('btn-1');
@@ -62,39 +62,56 @@ let btn3 = document.getElementById('btn-3');
 let listadoCarrito = document.getElementById('carrito');
 let total = document.getElementById('total');
 
+let carrito =[];
+
 btn1.onclick = function (){
     let precio = document.getElementById('precio-1').innerText;
     console.log(precio)
-}
-
-
-//*Agregar producto al carrito*/
-const agregarProductoAlCarrito = (precioProducto, producto)=>{
-    let precio = document.getElementById(precioProducto).innerText;
-    let posicion = carrito.findIndex(elem => elem.producto === producto);
+    let posicion = carrito.findIndex(elem => elem.producto === 'cayetana');
     console.log(posicion)
     if(posicion!=-1){
-        carrito[posicion].cantidad += 1;
+        console.log('entra en -1')
+        carrito[posicion].cantidad = carrito[posicion].cantidad + 1;
     }else{
-        carrito.push({producto: producto, precio:precio, cantidad: 1});
+        carrito.push({producto: 'cayetana', precio:precio, cantidad: 1});
     }
     renderizarElemento()
 }
+
 const renderizarElemento = ()=>{
-    listadoCarrito.innerHTML = '';
-    let sumaTotal =0;
-    if(carrito.lenght > 0){
-        for (let index = 0; index >carrtio.length; index++){
+    // let elemento = document.createElement('div') 
+     elemento.innerHTML = `<p>
+                            Producto: ${carrito[0].producto}
+                            Precio; ${carrito[0].precio}</p>`;
+    //listadoCarrito.append(elemento)
+    let sumaTotal = 0;
+    if(carrito.length > 0){
+        for (let index = 0; index > carrito.length; index++) {
             let elemento = document.createElement('div')
-            elemento.innerHTML = <P>
-                                producto: ${carrito[index].producto}
-                                cantidad: ${carrito[index].cantidad}</P>;
-            sumaTotal = sumaTotal + carrito[index].precio * carrito[index].cantidad
-            listadoCarrito.append(elemento);
-            sumaTotal.innerText = 'Total a pagar: ${sumaTotal}';
+            elemento.innerHTML = `<p>
+                                    Producto: ${carrito[index].producto}
+                                    Cantidad: ${carrito[index].cantidad}</p>`;
+        sumaTotal = sumaTotal + carrito[index].precio * carrito[index].cantidad;
+        listadoCarrito.append(elemento);
+        total.innerText `Total a pagar: ${sumaTotal}`
         }
     }
 }
+ const agregarProductoAlCarrito = (precioProducto,producto)=>{
+    let precio = document.getElementById('precioProducto').innerText;
+    let posicion = carrito.findIndex(elem => elem.producto === 'cayetana');
+    console.log(posicion)
+    if(posicion!=-1){
+        console.log('entra en -1')
+        carrito[posicion].cantidad = carrito[posicion].cantidad + 1;
+    }else{
+        carrito.push({producto: 'cayetana', precio:precio, cantidad: 1});
+    }
+    renderizarElemento()
+    agregarProductoAlCarrito('precio-3','precio-3')
+
+ }
+
 
 //* fecha */
 const hoy = new Date ("April 06, 2022");
