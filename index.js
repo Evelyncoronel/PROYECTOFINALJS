c4e7c98bcd2b8e7af7166188a1bcefe180cf9550
malbec <evelyn.ec.dc@gmail.com>
2022-04-20 20:42:31 -0300

    se eliminan archivos js, se intenta arreglar carrito y carga de new products

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 //* Bienvenida */
 
-let textoA = 'Bienvenida a Baraco';
+//*let textoA = 'Bienvenida a Baraco';
 let nombreIngresado= prompt ('Ingrese su nombre');
 console.log ('Ingrese su nombre');
-
-let unir= textoA + nombreIngresado
+let unir= textoA + nombreIngresado;//
 
 /** Arrays */
 const talles = [1,2,3,4]
@@ -30,29 +29,16 @@ if(enStock) {
     console.log ("Sin stock");
 };
 
-
-function descuento () {
-    //* funcion de descuento.
+//*FUNICION DE DESCUENTO*/
+function descuento (precio, porcentaje) {
+    return precio + ((precio * porcentaje) / 100) //* funcion de descuento.
+}
+for (let index = 0; index > 5; index++) {
+    let resultado = descuento(parseFloat(prompt("INGRESAR PRECIO"))
+                    , parseFloat(prompt("INGRESAR %")));
+    alert(resultado);
 }
 descuento()
-
-while (producto) {
-    let nombreProducto = prompt('Que modelo eliges: Aruba, Malibu, Positano');
-    let talleproducto = prompt('Elige talle: small, medium, large, xtralarge');
-    let colorProducto = prompt('negro, lila, azul, verde');
-    
-    if(producto == 'aruba'+ talleproducto == 'small' + colorProducto == 'lila'){ 
-        alert('disponible');
-    }else(producto == 'aruba' + talleproducto == 'large' + colorProducto == 'lila'){
-        alert('sin stock');
-    }
-    if(producto == 'aruba'+ talleproducto == 'small' + colorProducto == 'negro'){ 
-        alert('disponible');
-    }else(producto == 'aruba' + talleproducto == 'medium' + colorProducto == 'negro'){
-        alert('sin stock');
-    }
-}
-console.log("producto"); 
 
 //*Carrito con DOM*/
 let btn1 = document.getElementById('btn-1');
@@ -82,7 +68,7 @@ btn1.onclick = function (){
 btn2.onclick = function (){
     let precio = document.getElementById('precio-2').innerText;
     console.log(precio)
-    let posicion = carrito.findIndex(elem => elem.producto === 'cayetana');
+    let posicion = carrito.findIndex(elem => elem.producto === 'london');
     console.log(posicion)
     if(posicion!=-1){
         console.log('entra en -2')
@@ -93,7 +79,20 @@ btn2.onclick = function (){
     renderizarElemento()
     agregarProductoAlCarrito('precio-2','london')
 }
-
+btn3.onclick = function (){
+    let precio = document.getElementById('precio-3').innerText;
+    console.log(precio)
+    let posicion = carrito.findIndex(elem => elem.producto === 'LONDON');
+    console.log(posicion)
+    if(posicion!=-1){
+        console.log('entra en -3')
+        carrito[posicion].cantidad = carrito[posicion].cantidad + 1;
+    }else{
+        carrito.push({producto: 'London', precio:precio, cantidad: 1});
+    }
+    renderizarElemento()
+    agregarProductoAlCarrito('precio-3','london')
+}
 
 
 
@@ -135,3 +134,17 @@ const renderizarElemento = ()=>{
 
 //* fecha */
 const hoy = new Date ("April 06, 2022");
+
+class Products {
+    set1 (nombre, precio, talle, color) {
+        this.nombre = nombre;
+        this.precio = parseFloat (precio);
+        this.talle = talle;
+        this.color = color;
+    }
+}
+const Products = [];
+Products.push (new products ("cayetana", 1970, [small, medium, large], [rojo, blanco, negro]));
+Products.push (new products ("london", 1870, [small, medium, large], [ blanco, negro]));
+Products.push (new products ("london", 1970, [small, medium, large], [verde-ingles]));
+localStorage.setItem('products', JSON.stringify(products))
